7c74e22d08bd0c2eebf0ad4c5775ba9a625b9417
Orifjon <orifjonokeng@gmail.com>
2024-12-06 10:23:40 +0500

    template add

--- main/urls.py
+++ main/urls.py
@@ -1,3 +1,14 @@
+from django.urls import path 
+
+from . import views
+
+app_name = "blog"
+
 urlpatterns = [
+    path('', views.home_page, name='home'),
+    path('detail/<post_id>', views.detail, name='detail'),
+    path('rating/<value>/<post_id>', views.set_rating, name='set_rating'),
     
+    path("<category_slug>/posts", views.category_list, name='category_list'),
+    path("search/", views.search, name="search")
 ]
